97338d735d2303bf93f4caf3930d4a638b7f2be6
JoseParedezz <joseparedez599@gmail.com>
2025-06-20 17:40:40 -0300

    Update sp_listar_venta_por_usuario.sql

--- procedimientos/sp_listar_venta_por_usuario.sql
+++ procedimientos/sp_listar_venta_por_usuario.sql
@@ -3,9 +3,14 @@ create procedure sp_listar_venta_por_usuario(
 )
 as
 begin
-	select V.id_venta, V.fecha_hora, V.importe_total, V.metodo_pago,
-	U.nombre_usuario, U.apellido_usuario from Venta V
-	inner join  Usuario U on V.DNI_usuario = U.DNI_usuario
-	where u.DNI_usuario = @DNI_usuario
-	order by v.fecha_hora desc
+	begin try
+		select V.id_venta, V.fecha_hora, V.importe_total, V.metodo_pago,
+		U.nombre_usuario, U.apellido_usuario from Venta V
+		inner join  Usuario U on V.DNI_usuario = U.DNI_usuario
+		where u.DNI_usuario = @DNI_usuario
+		order by v.fecha_hora desc
+	end try
+	begin catch
+		
+	end catch
 end
