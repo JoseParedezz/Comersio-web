616ad7999d2e1c0b4d6dcd4a9725e78cd72f4360
JoseParedezz <joseparedez599@gmail.com>
2025-06-21 01:55:01 -0300

    Update sp_buscar_stock.sql

--- procedimientos/sp_buscar_stock.sql
+++ procedimientos/sp_buscar_stock.sql
@@ -3,26 +3,21 @@ GO
 CREATE PROCEDURE sp_buscar_stock_productos(
     @stock INT
 )
--- Inicio del procedimiento:
 AS 
 BEGIN
    
     begin try
-     --Traemos los productos
-        SELECT
+        select
             id_Producto,
             nombre_producto,
             precio_venta,
             stock,
             id_categoria,
             id_proveedor
-        FROM Producto
-        WHERE stock < @stock;
-    END try
+        from Producto
+        where stock < @stock;
+    end try
     begin catch
         print error_message()
     end catch
-END;
-
---Probamos
-EXEC sp_buscar_stock_productos 20;
+end;
